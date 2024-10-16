b370de67b5838b9160faebdcb9d474e1b9d7bea7
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-10-16 14:45:18 +0100

    Learning to create webservices using Java. I hope to use this new aquired skill to host my portfolio in the future

---
+++ HelloFromWebServices/src/main/java/com/mycompany/hellofromwebservices/HellloWorldResource.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package com.mycompany.hellofromwebservices;
+
+
+
+
+/**
+ * REST Web Service
+ *
+ * @author dmoc2
+ */
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/hello")
+public class HellloWorldResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello, World from Web Service!";
+    }
+    
+    @GET
+    @Path("/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        
+        return "<?xml version=\"1.0\"?>"+"<message>MY Hello World</message>";
+        //throw new UnsupportedOperationException();
+    }
+    
+    
+    @GET
+    @Path("/text")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText()
+    {
+        return "MY Hello World";
+        //throw new UnsupportedOperationException();
+    }
+    
+    
+    
+}
